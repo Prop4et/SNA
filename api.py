20cd09199dcf9194a58b8943a1d31f15bde3d5ab
Prop4et <francostanco97@gmail.com>
2022-05-13 12:16:28 +0200

    initial commit

---
+++ api.py
@@ -0,0 +1,14 @@
+import tweepy
+import configparser
+
+#read config
+config = configparser.ConfigParser()
+config.read('./config.ini')
+
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
+
+#auth with tweepy
