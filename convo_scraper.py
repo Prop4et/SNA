f94b758e4c3ae5111e8e0df10185c5d4d8c6ac4e
Prop4et <francostanco97@gmail.com>
2022-05-14 11:31:52 +0200

    conversation scraper added

---
+++ convo_scraper.py
@@ -0,0 +1,17 @@
+import api
+import pandas as pd
+
+def main():
+    df = pd.read_csv('data2.csv', dtype='str')
+    config_dict = api.read_config()
+    headers = api.create_headers(config_dict['bearer_token'])
+    api.writeHeader("data3.csv")
+    start_time = "2022-05-10T00:00:00.000Z"
+    for idx, row in df.iterrows():
+        print(row['author_id'], row['conversation_id'])
+        url = api.create_url("conversation_id:"+row['conversation_id']+" -is:retweet", start_time)
+        json_response = api.connect_to_endpoint(url[0], headers, url[1])
+        api.append_to_csv(json_response, "data3.csv")
+
+if __name__ == '__main__':
+    main()
